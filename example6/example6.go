dd41aeef0027aed0fe12d902c4a6e986804f92b1
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2021-10-08 16:45:51 +0300

    add examples

---
+++ example6/example6.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+func init() {
+	// Downgrading the number of available threads to avoid parallelism
+	// Single-thread go program in other words
+	runtime.GOMAXPROCS(2)
+}
+
+// Wait for result pattern
+func main() {
+	before := time.Now()
+	// Unbuffered channel init
+	ch := make(chan string)
+	go func() {
+		for i := 0; i < 10; i++ {
+			// Don't do this in production - it's for simulation/demo purposes ONLY!!
+			time.Sleep(time.Duration(rand.Intn(600)) + time.Millisecond)
+			println("sending signal...")
+			ch <- "ok"
+			println("`ok``signal sent!")
+		}
+		close(ch)
+	}()
+
+	for {
+		println("receiving signal...")
+		val, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Printf("`%s` signal received!\n\n\n", val)
+
+	}
+
+	after := time.Now()
+	fmt.Printf("exitting main program. time elapsed : %v\n", after.Sub(before))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-concurrency
+
+go 1.16
